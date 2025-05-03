3fd6c26511ba14f7273b2e244c63c2448af81719
Josh Newland <newlandj@gmail.com>
2025-05-03 15:09:27 -0700

    fix db conn string

--- app/db.py
+++ app/db.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql+asyncpg://user:password@localhost:5432/kids_story")
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://", 1)
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 More to come!
+
+Heroku Stuff:
+app url: https://kids-story-creator-5741befe1fe7.herokuapp.com/
